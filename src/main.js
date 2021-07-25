a3c99c10ea638be264d1d9d357a767835fb5bb86
noyce.s1 <noyce.s1@123.com>
2021-07-25 12:13:15 +0800

    增加移动端适配

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/styles/reset.css'
+import '@/styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello world
+
   </div>
 </template>
 
@@ -12,7 +12,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
