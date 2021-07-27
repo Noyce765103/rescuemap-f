6e78ae431587a2344d6586c644b69fa6b3ef9c7b
Hu wen bin <dum_my203@126.coom>
2021-07-28 01:17:20 +0800

    地图初步打底

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// 用于存放全局通用的 参数 与 状态标记
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 这里放全局参数
+    demoValue: {}
   },
   mutations: {
+    // set方法
+    setDemoValue (state, newValue) {
+      state.demoValue = newValue
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <pure-map></pure-map>
   </div>
 </template>
+<script>
+import PureMap from '../components/PureMap'
+
+export default {
+  components: { PureMap }
+}
+
+</script>
