6cd9366bd303556469cb22fd509eae253670c8b4
techming <hmeng29@wisc.edu>
2021-07-28 00:01:57 +0800

    feat: 创建InfoTab组件

---
+++ src/components/Home/InfoTab.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-tabs>
+    <el-tab-pane> </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+export default {
+  name: 'InfoTab',
+  props: {
+    rescueInfo: Array,
+  },
+}
+</script>
+
+<style></style>
