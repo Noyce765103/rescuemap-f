a340ec6d1860e8ca7738b74530ceb309b4ad2bef
Hu wen bin <dum_my203@126.coom>
2021-07-28 11:39:09 +0800

    地图基础引入

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,11 +1,37 @@
 <template>
-  <div></div>
+  <div class="map-container">
+    <h1>----------------------↓地图↓--------------------</h1>
+    <div :style="{width:'100%',height:'300px'}">
+      <el-amap vid="amap"
+               class="amap-demo"
+               :plugin="plugin"
+               :zoom="zoom"
+               :center="center">
+      </el-amap>
+    </div>
+    <h1>----------------------↑地图↑--------------------</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Map',
-};
+  data() {
+    const self = this
+    return {
+      zoom: 13, // 地图缩放等级,3-18,18最大
+      center: [121.59996, 31.197646], // 地图中央经纬度
+      lng: 0,
+      lat: 0,
+      loaded: false
+    }
+  },
+  name: 'Map'
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+#map-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
