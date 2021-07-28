c32db9cd857985573f1729b5343e4c6d75f58074
Hu wen bin <dum_my203@126.coom>
2021-07-28 11:39:09 +0800

    地图基础引入

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import fastClick from 'fastclick';
 import '@/assets/iconfont.css';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import VUEAmap from 'vue-amap';
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
@@ -17,3 +18,14 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+
+Vue.use(VUEAMap)
+
+// 初始化vue-amap
+VUEAMap.initAMapApiLoader({
+  // 高德key
+  key: '876a67525aa9bd4ca0455bbff07e5859',
+  // 插件集合 （插件按需引入）
+  plugin: ['AMap.Geolocation', 'AMap.ElasticMarker']
+})
