afd60fa9f7be1a02aec156a4c3968a7d3fa2bcfd
Hu wen bin <dum_my203@126.coom>
2021-07-30 00:00:06 +0800

    地图标记点实现分类

---
+++ src/components/Map/detailCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="info-card">
+    <el-col class="info-card-description" :xs="12">
+      <div class="info-card-description-title">姓名</div>
+      <div class="info-card-description-content">{{ data.name }}</div>
+    </el-col>
+    <el-col class="info-card-description" :xs="12">
+      <div class="info-card-description-title">被困人数</div>
+      <div class="info-card-description-content">
+        {{ data.trappedNumber }}
+      </div>
+    </el-col>
+
+    <el-col class="info-card-description" :xs="24">
+      <div class="info-card-description-title">联系电话</div>
+      <div class="info-card-description-content">
+        {{ data.phone }}
+      </div>
+    </el-col>
+
+    <el-col class="info-card-description" :xs="24">
+      <div class="info-card-description-title">位置</div>
+      <div class="info-card-description-content">
+        {{ data.location }}
+      </div>
+    </el-col>
+
+    <el-col class="info-card-description" :xs="24">
+      <div class="info-card-description-title">求助时间</div>
+      <div class="info-card-description-content">
+        {{ data.SOSTime }}
+      </div>
+    </el-col>
+
+    <el-col v-if="data.description != null" class="info-card-description" :xs="24">
+      <div class="info-card-description-title">详细描述</div>
+      <div class="info-card-description-content info-description-short">
+        {{ data.description }}
+      </div>
+    </el-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoCard',
+  props: {
+    data: Object,
+  },
+};
+</script>
+
+<style>
+.info-card {
+  box-shadow: 0 1px 4px 1px rgba(198, 198, 198, 0.2);
+  border: solid 2px whitesmoke;
+  border-radius: 10px;
+  min-height: 240px;
+  margin: 0 6px 20px 6px;
+  padding: 25px 20px 25px 15px;
+}
+.info-card-description {
+  display: flex;
+  flex-direction: row;
+  font-size: 14px;
+  line-height: 18px;
+  margin-bottom: 8px;
+}
+.info-card-description-title {
+  min-width: 65px;
+  text-align: right;
+  margin-right: 6px;
+  font-weight: 500;
+}
+
+.info-card-description-title::after {
+  content: ':';
+}
+.info-description-short {
+  font-size: 13px;
+  line-height: 16px;
+  display: -webkit-box;
+  -webkit-line-clamp: 6;
+  -webkit-box-orient: vertical;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+</style>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="map-container" v-on:load="init">
+    <div>
+      <div class="div-inline" style="color: black" :class="{'button-pressed':buttonOn[0]}" id="toggle-rescue-point"
+           @click="filterMarker(0)">
+        求救点
+      </div>
+      <div class="div-inline" style="color: black" :class="{'button-pressed':buttonOn[1]}" id="toggle-danger-zone"
+           @click="filterMarker(1)">
+        险情
+      </div>
+      <div class="div-inline" style="color: black" :class="{'button-pressed':buttonOn[2]}" id="toggle-shelter"
+           @click="filterMarker(2)">避难所
+      </div>
+      <div class="div-inline" style="color: black" :class="{'button-pressed':buttonOn[3]}" id="toggle-emergency-team"
+           @click="filterMarker(3)">救援队伍
+      </div>
+    </div>
     <h1>----------------------↓地图↓--------------------</h1>
     <div :style="{width:'100%',height:'300px'}">
       <el-amap vid="amap"
@@ -18,12 +34,13 @@ import {AMapManager, lazyAMapApiLoaderInstance} from 'vue-amap'
 // 与地图连接的manager实体,需要通过它获取原生AMap.Map对象
 const amapManager = new AMapManager();
 const dummyData = [
-  {uid: '00001', label: "marker1", pos: [121.596, 31.197646]},
-  {uid: '00002', label: "marker2", pos: [121.586, 31.197646]},
-  {uid: '00003', label: "marker3", pos: [121.576, 31.197646]},
-  {uid: '00004', label: "marker4", pos: [121.566, 31.197646]},
-  {uid: '00005', label: "marker5", pos: [121.556, 31.197646]},
-  {uid: '00006', label: "marker6", pos: [121.546, 31.197646]},
+    // markerType对应: 0->求助信息 1->险情 2->庇护所 3->救援队
+  {uid: '00001', label: "marker1", pos: [121.596, 31.197646], markerType: 0},
+  {uid: '00002', label: "marker2", pos: [121.586, 31.197646], markerType: 0},
+  {uid: '00003', label: "marker3", pos: [121.576, 31.197646], markerType: 1},
+  {uid: '00004', label: "marker4", pos: [121.566, 31.197646], markerType: 1},
+  {uid: '00005', label: "marker5", pos: [121.556, 31.197646], markerType: 2},
+  {uid: '00006', label: "marker6", pos: [121.546, 31.197646], markerType: 3},
 ];
 const markerList = [];
 export default {
@@ -34,7 +51,9 @@ export default {
       // 高德地图主参数
       amapManager,
       mapZoom: 14, // 默认放大等级,3-18,可变
-      mapCenter: [121.577, 31.196646], // 地图中央经纬度,可变
+      mapCenter: [121.577, 31.196646], // 默认地图中央经纬度,可变
+
+      buttonOn: [true, true, true, true]
     }
   },
 
@@ -51,33 +70,54 @@ export default {
     },
 
     initMap() {
-      alert("initializing map")
+      this.drawMarkers()
+    },
+    drawMarkers() {
       lazyAMapApiLoaderInstance.load().then(() => {
         // 获取原生高德API的map对象
         const map = this.amapManager.getMap();
         // 清空地图
+        map.clearMap();
         let marker;
-        // 创建所有标记点(marker)
+        // 创建标记点(marker)
         for (let i = 0; i < dummyData.length; i++) {
-          marker = new AMap.Marker({
-            position: dummyData[i].pos,
-            title: dummyData[i].label,
-            anchor: 'bottom-center',
-            label: {
-              content: dummyData[i].uid,
-              direction: 'right'
+          if (this.buttonOn[dummyData[i].markerType]) {
+            marker = new AMap.Marker({
+              position: dummyData[i].pos,
+              title: dummyData[i].label,
+              anchor: 'bottom-center',
+              label: {
+                content: dummyData[i].uid,
+                direction: 'right'
+              }
+            });
+            switch (dummyData[i].markerType) {
+              case 0:
+                marker.setIcon(require("@/assets/icon-fire-zone.png"));
+                break;
+              case 3:
+                marker.setIcon(require("@/assets/icon-rescue-team.png"))
             }
-          });
-          // 绑定单击
-          marker.on('click', this.eventClick)
-          // 放上地图
-          map.add(marker);
+            // 绑定单击
+            marker.on('click', this.eventClick)
+            // 放上地图
+            map.add(marker);
+          }
         }
       });
     },
+
+    filterMarker(buttonID) {
+      // 翻转开关
+      alert(buttonID)
+      alert(this.buttonOn)
+      this.buttonOn[buttonID] = !this.buttonOn[buttonID];
+      this.drawMarkers()
+    },
+
     // 回调函数,响应markers的单击事件
     eventClick(ev) {
-      alert("eventType:" + ev.type + "当前放大等级:"+this.mapZoom);
+      alert("eventType:" + ev.type + "当前放大等级:" + this.mapZoom);
       this.amapManager.getMap().panTo(ev.lnglat);
     }
   }
@@ -85,8 +125,16 @@ export default {
 </script>
 
 <style scoped>
-#map-container {
+.map-container {
   width: 100%;
   height: 100%;
+}
+
+.div-inline {
+  display: inline;
+}
+
+.button-pressed {
+  color: red;
 }
 </style>
